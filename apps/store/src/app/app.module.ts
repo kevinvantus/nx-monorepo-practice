a09cb1a8db3b19326cbc146e584f8569142a7161
kevinvantus <kevinvantus@gmail.com>
2023-01-10 07:15:42 +0100

    generated application and libraries

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ShopComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
